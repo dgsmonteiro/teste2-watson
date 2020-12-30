57c41233c720caa3d38beb66bd45dcc94fc09fee
Douglas Monteiro <douglasmonteiro@icloud.com>
2020-12-30 16:45:11 -0300

    update

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -4,7 +4,7 @@ function Footer() {
     <footer className="container mx-auto fixed bottom-0 right-0 left-0 flex justify-center items-center h-24">
       Desenvolvido por 
       <a href="https://douglasmonteiro.dev.br">
-      Douglas Monteiro
+       Douglas Monteiro
       </a>
     </footer>
   )
